a8d2e35e399129d6b8b2a54184258f2bcecd1d14
Carlos <toqueteos@gmail.com>
2012-11-04 21:36:15 +0100

    Updated Bot example. Still not working.

--- examples/bot.go
+++ examples/bot.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/toqueteos/ts3"
 )
 
 func main() {
-	t, err := ts3.Dial("127.0.0.1")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	t := ts3.Dial("127.0.0.1")
 
 	t.Cmd("help")
 	t.Cmd("login serveradmin 123456")
