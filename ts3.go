b2616c2dee605dd53f1634fc2766f548aad515a0
090317 <090317@nhtv.nl>
2014-05-05 18:55:25 +0200

    Big overhaul Rewrote most of the system to work better with command timing and notification handling

--- ts3.go
+++ ts3.go
@@ -1,17 +1,13 @@
-// TeamSpeak 3 Server Query library
-//
-// Reference: http://goo.gl/OpJXz
 package ts3
 
 import (
 	"bufio"
 	"bytes"
-	"fmt"
+	"errors"
 	"io"
-	stdlog "log"
 	"net"
-	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -21,23 +17,34 @@ const (
 )
 
 var (
-	// Custom logger
-	log = stdlog.New(os.Stdout, "ts3> ", stdlog.LstdFlags)
 	// ts3.Dial max timeout
-	DialTimeout = 1 * time.Second
+	DialTimeout    = 1 * time.Second
+	CommandTimeout = 500 * time.Millisecond
 )
 
-// Holds incoming, outgoing and notification requests (1) and responses (2 & 3)
-type RWChan struct{ In, Out, Not chan string }
+type notification func(string, string)
 
 type Conn struct {
-	conn net.Conn
-	rw   RWChan
+	conn          net.Conn
+	rbuf          *bufio.Reader
+	wbuf          *bufio.Writer
+	cResult       chan string
+	cNotification chan string
+	cError        chan ErrorMsg
+	notifyCb      notification
+	cmdSync       *sync.WaitGroup
+	cmdLast       time.Time
+	cmdTimeout    time.Duration
+}
+
+type ErrorMsg struct {
+	Id  int
+	Msg string
 }
 
 // Dial connects to a local/remote TS3 server. A default port is appended to
 // `addr` if user doesn't provide one.
-func Dial(addr string) *Conn {
+func Dial(addr string, whitelisted bool) (*Conn, error) {
 	var (
 		err  error
 		line string
@@ -50,108 +57,149 @@ func Dial(addr string) *Conn {
 
 	// Try to establish connection
 	conn, err := net.DialTimeout("tcp", addr, DialTimeout)
-	fatal(err, fmt.Sprintf("Connection error: %v\n", err))
+	if err != nil {
+		return nil, err
+	}
+
+	// Create buffers
+	rbuf := bufio.NewReader(conn)
+	wbuf := bufio.NewWriter(conn)
+
+	timeout := time.Duration(350 * time.Millisecond)
+	if whitelisted {
+		timeout = time.Duration(0 * time.Millisecond)
+	}
 
 	// Allocate connection object
 	ts3conn := &Conn{
-		conn: conn,
-		rw: RWChan{
-			In:  make(chan string),
-			Out: make(chan string),
-			Not: make(chan string),
-		},
+		conn:          conn,
+		rbuf:          rbuf,
+		wbuf:          wbuf,
+		cResult:       make(chan string),
+		cNotification: make(chan string),
+		cError:        make(chan ErrorMsg),
+		notifyCb:      nil,
+		cmdSync:       new(sync.WaitGroup),
+		cmdLast:       time.Now(),
+		cmdTimeout:    timeout,
 	}
 
-	rbuf := bufio.NewReader(conn)
-
-	// Buffer to read from TCP socket; Read first line
+	// Read VerificationID
 	line, err = rbuf.ReadString('\n')
-	fatal(err, "Couldn't identify server.")
-	
+
 	// Then check if it's a TS3 server
 	if !strings.Contains(line, VerificationID) {
-		log.Fatal("Not a TeamSpeak 3 server.")
+		return nil, errors.New("Invalid VerificationID")
 	}
 
-	// Show welcome message
+	// Read welcome message
 	line, err = rbuf.ReadString('\n')
-	fatal(err, "Couldn't recv welcome message.")
 
-	// Copy flow: writer (request) -> conn -> reader (response)
-	go cp(ts3conn, conn)
-	go cp(conn, ts3conn)
+	// Sync socket incoming data, to ts3conn
+	go ts3conn.sync()
 
-	return ts3conn
-}
-
-// Read reads data from buffer into p doubling any IAC chars found (0xff), more
-// info on RFC 854 (Telnet).  It returns the number of bytes read into p.
-func (conn *Conn) Read(p []byte) (int, error) {
-	b := []byte(<-conn.rw.In)
-	// Double IAC chars
-	bytes.Replace(b, []byte{0xff}, []byte{0xff, 0xff}, -1)
-	copy(p, b)
-	return len(b), nil
-}
-
-// Write writes the contents of p into the buffer. It returns the number of
-// bytes written.
-func (conn *Conn) Write(p []byte) (int, error) {
-	s := string(p)
-	conn.rw.Out <- s
-	return len(p), nil
+	return ts3conn, nil
 }
 
 // Close closes underlying TCP Conn to local/remote server.
-func (c *Conn) Close() error {
-	return c.conn.Close()
+func (this *Conn) Close() error {
+	return this.conn.Close()
 }
 
 // Cmd sends a request to a server and waits for its response.
-func (c *Conn) Cmd(cmd string) string {
-	var s string
+func (this *Conn) Cmd(cmd string) (string, ErrorMsg) {
+	//Make sure only 1 command runs at a time per connection
+	this.cmdSync.Wait()
+	this.cmdSync.Add(1)
 
-	c.rw.In <- cmd + "\n"
+	//Make sure we timeout nicely per command to avoid spamming
+	diff := time.Since(this.cmdLast)
+	if diff < this.cmdTimeout {
+		<-time.After(this.cmdTimeout - diff)
+	}
 
-	// Some commands output two lines
-	var end bool
-	for !end {
+	var (
+		result string   //Holds end result
+		temp   string   //Holds temp result
+		err    ErrorMsg //Holds error message
+	)
+
+	// Write the cmd to the socket, ending with a \n
+	this.send(cmd + "\n")
+
+	// Block on a channel that will recieve the socket READ that is NOT a notification or error
+	// Block on a channel that will recieve the socket READ that is NOT a notification or result
+	done := false
+	for !done {
 		select {
-		case line := <-c.rw.Out:
-			s += line
-			/*if strings.HasPrefix(s, "notify") {
-				c.rw.Not <- s
-				end = true
-			}*/
-			if strings.HasPrefix(s, "error id=") {
-				end = true
-			}
-		case <-time.After(500 * time.Millisecond):
-			end = true
+		case temp = <-this.cResult:
+			result += temp
+			continue
+		case err = <-this.cError:
+			done = true
+		case <-time.After(CommandTimeout):
+			//TODO Is there a better way to handle this?
+			err.Id = 1
+			err.Msg = "timeout"
+			done = true
 		}
 	}
 
-	return trimNet(s)
+	this.cmdLast = time.Now()
+	this.cmdSync.Done()
+	return result, err
 }
 
-// Chans returns a low-level interaction
-func (c *Conn) Chans() *RWChan {
-	return &c.rw
+func (this *Conn) NotifyFunc(cb notification) {
+	this.notifyCb = cb
+}
+
+func (this *Conn) send(p string) (int, error) {
+	b := []byte(p)
+	// Double IAC chars
+	bytes.Replace(b, []byte{0xff}, []byte{0xff, 0xff}, -1)
+	return this.conn.Write(b)
+}
+
+func (this *Conn) handleResponse(data string) {
+	// Has to be done by line!
+	// Sadly its possible, when spammed, to have an error and notification to stick together, and more
+	// Should be a better way to solve this overall, but teamspeak is a bitch and doesnt have a standard way to end a reply
+
+	lines := strings.Split(data, "\n")
+	var result string
+
+	for _, line := range lines {
+		switch {
+		case strings.HasPrefix(line, "error"):
+			this.cError <- parseError(line)
+		case strings.HasPrefix(line, "notify"):
+			if this.notifyCb != nil {
+				split := strings.SplitN(line, " ", 2)
+				go this.notifyCb(split[0], split[1])
+			}
+		default:
+			result += line
+		}
+	}
+
+	if result != "" {
+		this.cResult <- result
+	}
+}
+
+// Write writes the contents of p into the buffer. It returns the number of bytes written.
+func (this *Conn) Write(p []byte) (int, error) {
+	s := string(p)
+	s = strings.Replace(s, "\r", "", -1)
+
+	this.handleResponse(s)
+	return len(s), nil
 }
 
 // cp copies from an io.Reader to an io.Writer
-func cp(dst io.Writer, src io.Reader) {
-	_, err := io.Copy(dst, src)
-	fatal(err)
-}
-
-// fatal exits application if encounters an error
-func fatal(err error, s ...string) {
-	if err != nil {
-		if len(s) == 0 {
-			log.Fatal(err)
-		} else {
-			log.Fatal(s)
-		}
+func (this *Conn) sync() {
+	for {
+		io.Copy(this, this.conn)
 	}
 }
